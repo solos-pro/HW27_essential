60f13ec6f757b5d1b6dc23998d4b786aa78cdee2
solos-pro <sergey-solonichenko@rambler.ru>
2022-08-23 09:44:44 +0300

    HW30 init

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -42,10 +42,6 @@ INSTALLED_APPS = [
     'users',
 ]
 
-REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
